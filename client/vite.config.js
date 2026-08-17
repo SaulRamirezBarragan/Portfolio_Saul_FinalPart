f3863a7b26c7ecdb7dbfd8f9349e699e1e64ae30
SaulRamirezBarragan <saulramirezb@hotmail.com>
2026-08-17 03:51:35 -0400

    Render config

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,11 +8,11 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:`http://localhost:${PORT}`,
+        target:`http://127.0.0.1:${PORT}`,
         changeOrigin: true,
       },
       '/auth': {
-        target:`http://localhost:${PORT}`,
+        target:`http://127.0.0.1:${PORT}`,
         changeOrigin: true,
       },
     },

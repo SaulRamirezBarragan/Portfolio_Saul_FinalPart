f6cb1706b18050f8fc14d6164ca0d213772a07e2
SaulRamirezBarragan <saulramirezb@hotmail.com>
2026-08-17 03:15:46 -0400

    Render setup

--- server/express.js
+++ server/express.js
@@ -9,8 +9,11 @@ import projectRoutes from "./routes/project.routes.js";
 import contactRoutes from "./routes/contact.routes.js";
 import educationRoutes from "./routes/education.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+import path from "path";
 
 const app = express();
+const CURRENT_WORKING_DIR = process.cwd();
+app.use(express.static(path.join(CURRENT_WORKING_DIR, "dist/app")));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());

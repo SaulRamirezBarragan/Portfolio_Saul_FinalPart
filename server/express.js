4de388d8e7c351c6f15d774483b6354900ad9f11
SaulRamirezBarragan <saulramirezb@hotmail.com>
2026-08-17 03:58:55 -0400

    Render config final

--- server/express.js
+++ server/express.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+  origin: process.env.CLIENT_ORIGIN || "http://127.0.0.1:3000",
   credentials: true,
 }));
 app.use("/", userRoutes);
